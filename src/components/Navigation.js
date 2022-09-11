43f9c87bc80d2f7da236a43dcddbca4f426ee7dc
swaissa <104677486+swaissa@users.noreply.github.com>
2022-09-11 15:48:26 +0100

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        <div className='nav'>
+            <div>
+                <img src='https://play-lh.googleusercontent.com/vah-llpBVT6OVatZ_Ux8LlHFsbD9RTK4QnSVh6XAjjD3AaNq3MYCHxf1E1-L1GHVRTU' alt='RIEN'></img>
+            </div>
+<ul>
+  <il>home</il>
+  <il>Tv shows</il>
+
+  <il>Movies</il>
+
+  <il>Latest</il>
+  <il>My liste</il>
+
+
+
+</ul>
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Navigation
